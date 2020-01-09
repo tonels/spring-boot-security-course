50e4028111beb7356b0e22921b30c4f3cdbc4b96
liangshuai <1589900136@qq.com>
2020-01-09 20:25:27 +0800

    调整

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,13 +2,16 @@ package com.example.demo.student;
 
 public class Student {
 
-    private final Integer studentId;
-    private final String studentName;
+    private /*final*/ Integer studentId;
+    private /*final*/ String studentName;
 
     public Student(Integer studentId,
                    String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
+    }
+
+    public Student() {
     }
 
     public Integer getStudentId() {
